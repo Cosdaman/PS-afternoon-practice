d7082936882705b7b96f14ca1cc2e04d70495349
Cosdaman <kjc.ang@gmail.com>
2022-06-27 14:37:37 -0700

    figured out the last question

--- lambda_excercise.py
+++ lambda_excercise.py
@@ -1,3 +1,4 @@
+from functools import reduce
 # Lambda Excercise 1
 
 # Consider the List
@@ -10,16 +11,16 @@ prog_lang = [('Python', 3.8),
 
 # 1. Sort the list by each language's version in ascending order.
 #[('Scala', 2.13), ('Python', 3.8), ('Java', 13), ('JavaScript', 2019)]
-
-prog_lang.sort(key=lambda x: x[1])
-print(prog_lang)
+prog_lang_clone = prog_lang.copy()
+prog_lang_clone.sort(key=lambda x: x[1])
+print(prog_lang_clone)
 
 
 # 2. Sort the list by the length of the name of each language in descending order.
 #[('JavaScript', 2019), ('Python', 3.8), ('Scala', 2.13), ('Java', 13)]
 
-prog_lang.sort(key=lambda x: len(x[0]), reverse=True)
-print(prog_lang)
+prog_lang_clone.sort(key=lambda x: len(x[0]), reverse=True)
+print(prog_lang_clone)
 
 # 3. Filter the list so that it only contains languages with 'a' in it.
 #[('Java', 13), ('JavaScript', 2019), ('Scala', 2.13)]
@@ -42,3 +43,7 @@ print(prog_lang_lower)
 # 6. Generate a tuple in the form,
 #("All languages separated by commas","All versions separated by commas")
 #('Python,Java,JavaScript,Scala', '3.8,13,2019,2.13')
+
+prog_lang_tuple = reduce(
+    lambda x, y: (x[0]+','+y[0], str(x[1])+','+str(y[1])), prog_lang)
+print(prog_lang_tuple)
