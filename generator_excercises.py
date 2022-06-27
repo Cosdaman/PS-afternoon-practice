794494b1c0753fc98e755e74f9687b5f2814ac29
Cosdaman <kjc.ang@gmail.com>
2022-06-27 13:09:06 -0700

    primes completed

--- generator_excercises.py
+++ generator_excercises.py
@@ -1,21 +1,33 @@
-#Generator Excercises
+# Generator Excercises
 
-#Problem 1
-#Create a generator, primes_gen that generates prime numbers starting from 2.
+# Problem 1
+# Create a generator, primes_gen that generates prime numbers starting from 2.
+
+def primes_gen():
+    for num in range(20000):
+        if num > 1:
+            for i in range(2, num):
+                if (num % i) == 0:
+                    break
+            else:
+                yield num
+
 
 gen = primes_gen()
 for _ in range(10):
     print(next(gen), end=' ')
+
+
 # Expected output
 # 2 3 5 7 11 13 17 19 23 29
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 
-#Problem 2
-#Create a generator, unique_letters that generates unique letters from
-#the input string. It should generate the letters in the same order as
-#from the input string.
+# Problem 2
+# Create a generator, unique_letters that generates unique letters from
+# the input string. It should generate the letters in the same order as
+# from the input string.
 
-for letter in unique_letters('hello'):
-    print(letter, end=' ')
+# for letter in unique_letters('hello'):
+#     print(letter, end=' ')
 # Expected output
 # h e l o
